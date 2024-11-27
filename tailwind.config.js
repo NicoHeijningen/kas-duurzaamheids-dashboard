f06a350e1407ba87547f4b970d18b5bea5ee045f
Your Name <you@example.com>
2024-11-27 02:00:52 +0100

    working

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,8 @@ module.exports = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
-  prefix: "",
+    './src/**/*.{ts,tsx,js,jsx}',
+	],
   theme: {
     container: {
       center: true,
@@ -59,12 +58,12 @@ module.exports = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: "0" },
+          from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+          to: { height: 0 },
         },
       },
       animation: {
